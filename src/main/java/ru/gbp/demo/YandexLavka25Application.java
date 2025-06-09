72af3a6a02c6e3a850e99bebb3c4eb49c7905cd6
Admin <borisgubanov039@gmail.com>
2025-06-09 20:00:15 +0300

    init commit

---
+++ src/main/java/ru/gbp/demo/YandexLavka25Application.java
@@ -0,0 +1,13 @@
+package ru.gbp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexLavka25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YandexLavka25Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gbp/demo/YandexLavka25ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gbp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexLavka25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
