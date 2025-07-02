66f9425b292c6c24da9e9fbbb8267fd8047a6858
p0omb <borisgubanov039@gmail.com>
2025-07-03 00:03:14 +0300

    introduce OrderRepository into OrdersService

--- src/main/java/ru/gbp/demo/service/OrdersService.java
+++ src/main/java/ru/gbp/demo/service/OrdersService.java
@@ -1,26 +1,21 @@
 package ru.gbp.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import ru.gbp.demo.model.OrderDTO;
+import ru.gbp.demo.repository.OrderRepository;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @Repository
 public class OrdersService {
-    // TODO Внедрить ордер репозетори
-
-//    public interface MyRepository extends JpaRepository<MyEntity, Long> {
-//
-//        @Query("SELECT e FROM MyEntity e ORDER BY e.someField ASC")
-//        List<MyEntity> findAllSortedBySomeFieldAsc();
-//
-//        @Query("SELECT e FROM MyEntity e ORDER BY e.anotherField DESC")
-//        List<MyEntity> findAllSortedByAnotherFieldDesc();
-//    }
+    @Autowired
+    private OrderRepository orderRepository;
 
     public List<OrderDTO> getOrders() {
         return List.of(
