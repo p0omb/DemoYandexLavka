16ed12b28a7b758d2f575be0fc95ec70858d6711
p0omb <borisgubanov039@gmail.com>
2025-07-04 01:39:06 +0300

    rename OrderDTO to OrderDto in OrdersController

--- src/main/java/ru/gbp/demo/controller/OrdersController.java
+++ src/main/java/ru/gbp/demo/controller/OrdersController.java
@@ -1,14 +1,15 @@
 package ru.gbp.demo.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.gbp.demo.model.OrderDTO;
+import ru.gbp.demo.model.OrderDto;
 import ru.gbp.demo.service.OrdersService;
-
 import java.util.List;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/orders")
@@ -18,12 +19,21 @@ public class OrdersController {
     private final OrdersService ordersService;
 
     @GetMapping
-    public List<OrderDTO> orders() {
+    public List<OrderDto> orders() {
       return ordersService.getOrdersTest();
     }
-
+////    GET /couriers
+//    @GetMapping(produces = APPLICATION_JSON_VALUE)
+//    public List<courierResponce> findAll() {
+//        return userService.findAll();
+//    }
+////    GET /couriers/{courier_id}
+//    @GetMapping(value = "/{courier_Id}", produces = APPLICATION_JSON_VALUE)
+//    public UserResponse findById(@PathVariable Integer userId) {
+//        return userService.findById(userId);
+    }
 
 //    public ResponseEntity<List<Orders>> getOrders() {
 //        return ResponseEntity.ok().body(ordersService.getAllOrders());
 //    }
-}
+//}
