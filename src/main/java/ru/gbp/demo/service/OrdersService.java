33408f7053c816ced8700531088091fe03c57fb1
p0omb <borisgubanov039@gmail.com>
2025-07-03 18:11:24 +0300

    add receiving orders from the DB and converting Entity to DTO in OrdersService

--- src/main/java/ru/gbp/demo/service/OrdersService.java
+++ src/main/java/ru/gbp/demo/service/OrdersService.java
@@ -18,7 +18,32 @@ public class OrdersService {
     @Autowired
     private OrderRepository orderRepository;
 
+    //Получение заказов из БД
     public List<OrderDTO> getOrders() {
+
+        // 1)Получение всех сущностей из БД
+        List<Orders> ordersFromDB = orderRepository.findAll();
+
+        // 2)Конвертация каждой сущности в DTO
+        return ordersFromDB.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
+    //Конвертация Сущности в DTO
+    private OrderDTO convertToDTO(Orders orderEntity) {
+        return new OrderDTO(
+                orderEntity.getOrderId(),
+                orderEntity.getWeight(),
+                orderEntity.getRegions(),
+                orderEntity.getDeliveryHours(),
+                orderEntity.getCost(),
+                orderEntity.getCompletedTime()
+        );
+    }
+
+
+    //Тестовые данные
+    public List<OrderDTO> getOrdersTest() {
         return List.of(
                 new OrderDTO(1L, 1F, 1, List.of("15:12-16:12"), 1, LocalDateTime.now()),
                 new OrderDTO(3412144L, 4.321F, 54, List.of("12:30-14:50"), 8790, LocalDateTime.now())
