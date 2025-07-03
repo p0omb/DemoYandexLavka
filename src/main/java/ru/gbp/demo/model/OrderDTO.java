fee5afd24e03ae61e84546a22fb17e5c69930521
p0omb <borisgubanov039@gmail.com>
2025-07-03 21:43:36 +0300

    fix OrderDTO

--- src/main/java/ru/gbp/demo/model/OrderDTO.java
+++ src/main/java/ru/gbp/demo/model/OrderDTO.java
@@ -12,22 +12,10 @@ import java.util.List;
 @EqualsAndHashCode
 @RequiredArgsConstructor
 public class OrderDTO {
-
-
     private final Long orderId;
-
-
     private final Float weight;
-
-
     private final Integer regions;
-
-
     private List<String> deliveryHours = new ArrayList<>();
-
-
     private final Integer cost;
-
-
     private final LocalDateTime completedTime;
 }
