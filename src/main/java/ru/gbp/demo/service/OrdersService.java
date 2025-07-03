672a3fef66a5a3ab604273fc28022057507ce47f
p0omb <borisgubanov039@gmail.com>
2025-07-03 15:44:26 +0300

    fix test data of OrdersService

--- src/main/java/ru/gbp/demo/service/OrdersService.java
+++ src/main/java/ru/gbp/demo/service/OrdersService.java
@@ -1,6 +1,7 @@
 package ru.gbp.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.jaxb.SpringDataJaxb;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,14 +13,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
-@Repository
+//@Repository
 public class OrdersService {
     @Autowired
     private OrderRepository orderRepository;
 
     public List<OrderDTO> getOrders() {
         return List.of(
-                new OrderDTO(1L, 1F, 1, 2, LocalDateTime.now())
+                new OrderDTO(1L, 1F, 1, List.of("15:12-16:12"), 1, LocalDateTime.now()),
+                new OrderDTO(3412144L, 4.321F, 54, List.of("12:30-14:50"), 8790, LocalDateTime.now())
         );
     }
 }
