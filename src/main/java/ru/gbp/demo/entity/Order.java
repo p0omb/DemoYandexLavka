37756bf913f319cc434731d42f2876b919f1aac1
p0omb <borisgubanov039@gmail.com>
2025-07-05 00:28:07 +0300

    fix Order.java

--- src/main/java/ru/gbp/demo/entity/Order.java
+++ src/main/java/ru/gbp/demo/entity/Order.java
@@ -1,7 +1,8 @@
 package ru.gbp.demo.entity;
 
-
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -21,26 +22,28 @@ public class Order {
     private Integer orderId;
 
     @Column(nullable = false)
+    @Positive(message = "Weight must be positive")
     private Float weight;
 
     @Column(nullable = false)
+    @Positive(message = "Region must be positive")
     private Integer regions;
 
     @ElementCollection
-    @CollectionTable(name = "order_delivery_hours",
-            joinColumns = @JoinColumn(name = "order_id"))
+    @CollectionTable(name = "order_delivery_hours", joinColumns = @JoinColumn(name = "order_id"))
     @Column(name = "delivery_hours")
+    @NotEmpty(message = "Delivery hours cannot be empty")
     private List<String> deliveryHours = new ArrayList<>();
 
     @Column(nullable = false)
+    @Positive(message = "Cost must be positive")
     private Integer cost;
 
 //    @Column(name = "completed_time")
 //    private LocalDateTime completedTime;
 
-//    @Column(name = "assiwgned_courier_id")
+//    @Column(name = "assigned_courier_id")
 //    private Long assignedCourierId;;
-
 
     //    Конструктор для создания из DTO
     public Order(Float weight, Integer regions, List<String> deliveryHours, Integer cost) {
